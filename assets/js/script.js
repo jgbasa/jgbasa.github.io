f0f849c4fb89958863be66fc1ba3214a8c547452
Joan Gabrielle Basa <jgbasa@uwaterloo.ca>
2025-09-01 21:45:45 -0400

    add new web template

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// Example: smooth scroll (can expand later)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href"))
-        .scrollIntoView({ behavior: "smooth" });
-    });
-  });
-  
